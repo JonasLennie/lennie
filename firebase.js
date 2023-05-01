adb112c49f6589d2686ffd221512165376e1e703
JonasLennie3 <jonas.lennie.21@ucl.ac.uk>
2023-05-01 21:34:13 +0100

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHDkvg52ahS6qIvyStuRP_B6a31JSC_yI",
+  authDomain: "lennie-99c26.firebaseapp.com",
+  projectId: "lennie-99c26",
+  storageBucket: "lennie-99c26.appspot.com",
+  messagingSenderId: "992023055512",
+  appId: "1:992023055512:web:ea49323ad13d9b57e354ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
