fd96a72f801add5bfa2ffdcbb1ba0bb372937706
JonasLennie3 <jonas.lennie.21@ucl.ac.uk>
2023-05-01 21:29:32 +0100

    len

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import len from './lennie.gif'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{marginBottom: 100}}>
+          LENN.IE!!!!!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={len} className="App-logo" alt={"len"}/>
       </header>
     </div>
   );
